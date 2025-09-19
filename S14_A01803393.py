434d3288a0668c42bda31787c3dd0c1044d5b000
marinero767 <a01803393@tec.mx>
2025-09-19 09:52:34 -0600

    Create S14_A01803393.py

---
+++ S14_A01803393.py
@@ -0,0 +1,21 @@
+#Ejercicio1
+edad = int (input("Ingresa tu edad"))
+
+if edad <18:
+  print("Eres adulto")
+else:
+print("No eres adulto")
+
+#Ejercisio2
+edad = int(input("Ingresa tu edad"))
+
+if 13 < edad < 19:
+  print("Eres adolecente")
+else:
+  print("No eres adolecente")
+
+#Ejercicio3
+añoNacimiento = int(input("Ingresa tu año de nacimiento"))
+añoActual = 2025
+edad = añoActual - añoNacimiento
+print("Tienes ,edad, "años.")
