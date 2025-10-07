0aa7fb9592a589cd17fca1dd56b7468d29e68187
marinero767 <a01803393@tec.mx>
2025-10-06 20:49:11 -0600

    Update s18_A01803393.py

--- s18_A01803393.py
+++ s18_A01803393.py
@@ -1,3 +1,5 @@
+
+#EJERCIO 1
 temp = [15,15,17,30,25,30,30,30,31,32,30,36,38]
 media = sum(temp) / len(temp)
 
@@ -5,7 +7,7 @@ for i in temp:
   if i  > media:
     print(i, "esta arriba")
 
-
+#EJERCIO 2
 alumnos = ["reymundo,iker,giovani,santiago"]
 calificaciones = ["50,60,40,28"]
 promedio = sum(calificaciones) / lent(calificaciones)
@@ -14,7 +16,7 @@ for i in calif:
   if i > 70:
     print(i "esta arriba de 70")
 
-
+#EJERCIO 3
 compras = ["leche,pan,huevos,manzana"]
 ya_comprado = [False , False , False, False]
 
@@ -28,4 +30,61 @@ print("Estados de la lista de compras:")
 for i in range(len(compras)):
   estado = "* comprado" if ya_comprado[i] else "+ pendiente"
 print(f"{compras[i]} - {estado}")
+
+#Ejercio 4
+numeros = [45, 12, 89, 33, 7, 56, 72, 18, 90]
+
+maximo = numeros[0]
+minimo = numeros[0]
+
+for n in numeros:
+  if n > maximo:
+    maximo = n
+  if n < minimo:
+    minimo = n
+
+ordenados = sorted(numeros)
+
+print("Lista original:", numeros)
+print("Número máximo:", maximo)
+print("Número mínimo:", minimo)
+print("Lista ordenada:", ordenados)
+
+
+
+
+#EJERCIO 5
+numeros = [12, 5, 7, 8, 14, 21, 33, 40, 42, 55]
+pares = []
+impares = []
+
+for n in numeros:
+  if n % 2 == 0:
+    pares.append(n)
+  else:
+    impares.append(n)
+
+print("Pares:", pares)
+print("Impares:", impares)
+
+
+#EJERCIO 5
+
+usuarios = ["ana", "luis", "carlos", "maria", "sofia", "ana"]
+
+# Elimina nombres repetidos
+usuarios = list(set(usuarios))
+print("Lista actual de usuarios:", usuarios)
+
+while True:
+  nuevo = input("Ingresa un nombre de usuario: ")
+
+  if nuevo in usuarios:
+    print("Ese nombre ya existe, intenta con otro.")
+  else:
+    usuarios.append(nuevo)
+    print("Usuario agregado correctamente.")
+    break
+
+print("Lista final de usuarios:", usuarios)
      
