650edb19e707774bab1e24405e47982be2200e8e
marinero767 <a01803393@tec.mx>
2025-10-13 19:38:41 -0600

    Create s19_a01803393.py

---
+++ s19_a01803393.py
@@ -0,0 +1,37 @@
+import random 
+eleccionComputadora = random.choice (['Piedra', 'Papel', 'Tijera'])
+
+print (eleccionComputadora)
+
+eleccionJugador = input("quieres piedra papel o tijera ?")
+if eleccionComputadora == eleccionJugador:
+    print ("Empate!")
+if eleccioncomputadora=="tijera" and eleccionjugador== "piedra"
+  print("EL jugador gana!")
+
+if eleccioncomputadora== "papel" and elleccionjugador=="tijera"
+print("el jugador gana")
+
+ if eleccioncomputadora== "piedra" and elleccionjugador=="tijera"
+print("gana computadora")
+
+ if eleccioncomputadora== "piedra" and elleccionjugador=="papel"
+print("gana el jugador")
+
+
+ if eleccioncomputadora== "tijera" and elleccionjugador=="papel"
+print("gana computadora")
+
+
+ if eleccioncomputadora== "papel" and elleccionjugador=="piedra "
+print("gana computadora")
+
+ if eleccioncomputadora== "papel" and elleccionjugador=="papel"
+print("gana computadora")
+
+
+
+
+
+
+
